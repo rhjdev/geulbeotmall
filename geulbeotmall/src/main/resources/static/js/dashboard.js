979c16ffd83fc4eef371636c426a8895920311ba
rhjdev <rhjdev@gmail.com>
2022-07-01 22:04:57 +0900

    Feat: 대시보드 기본 구성

--- geulbeotmall/src/main/resources/static/js/dashboard.js
+++ geulbeotmall/src/main/resources/static/js/dashboard.js
@@ -22,5 +22,4 @@ window.addEventListener('DOMContentLoaded', event => {
             localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
         });
     }
-
 });
