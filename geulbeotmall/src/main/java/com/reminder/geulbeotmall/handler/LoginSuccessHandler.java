10f76b4454b828a222daad93d82ea5a1fb29c25b
rhjdev <rhjdev@gmail.com>
2022-08-12 19:52:32 +0900

    Feat: 로그인 요청 상황에 따른 페이지 리다이렉션 추가

--- geulbeotmall/src/main/java/com/reminder/geulbeotmall/handler/LoginSuccessHandler.java
+++ geulbeotmall/src/main/java/com/reminder/geulbeotmall/handler/LoginSuccessHandler.java
@@ -14,6 +14,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.RequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
@@ -68,10 +71,10 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 		LoopA:
 		for(int i=0; i < geulbeotCart.size(); i++) { //session 상품 목록을 DB에 추가
 			CartDTO cartDTO = geulbeotCart.get(i);
+			cartDTO.setMemberId(username);
 			
 			if(memberCart.size() == 0) {
 				memberCart = new ArrayList<>();
-				cartDTO.setMemberId(username);
 				memberCart.add(cartDTO);
 				cartMapper.addToCart(cartDTO);
 				continue;
@@ -79,24 +82,44 @@ public class LoginSuccessHandler implements AuthenticationSuccessHandler {
 			
 			for(int j=0; j < memberCart.size(); j++) {
 				if(geulbeotCart.get(i).getOptionNo() == memberCart.get(j).getOptionNo()) { //이미 담겨 있는 상품은 기존 목록에서 수량만 증가
-					memberCart.get(j).setQuantity(cartDTO.getQuantity());
+					int quantity = memberCart.get(j).getQuantity() + geulbeotCart.get(i).getQuantity();
+					cartDTO.setQuantity(quantity);
+					memberCart.get(j).setQuantity(quantity);
 					cartMapper.updateQuantityInCart(cartDTO.getMemberId(), cartDTO.getQuantity(), cartDTO.getOptionNo());
 					continue LoopA;
 				}
 			}
-			cartDTO.setMemberId(username);
 			memberCart.add(cartDTO);
 			cartMapper.addToCart(cartDTO);
 		}
 		session.setAttribute("geulbeotCart", memberCart);
 		session.setAttribute("countCartItem", memberCart.size());
 		
-		/* 3. 회원 권한에 따른 기본 페이지 이동 */
+		/* 3-1. 로그인 상황별 이동 경로 저장 */
+		RequestCache requestCache = new HttpSessionRequestCache();
+		SavedRequest savedRequest = requestCache.getRequest(request, response); //Spring Security가 요청을 가로채거든 URI 정보가 저장되는 객체
+		
+		String prevPage = (String) session.getAttribute("prevPage"); //MemberController로부터 저장돼 넘어온 사용자의 이전 경로
+		if(prevPage != null) session.removeAttribute("prevPage");
+		log.info("prevPage : {}", prevPage);
+		
+		String uri = request.getContextPath(); //uniform resource identifier
+		if(savedRequest != null) { //상황A. 접근한 페이지가 Security에 의해 로그인 인증을 필요로 한 경우
+			uri = savedRequest.getRedirectUrl();
+		} else if(prevPage != null) { //상황B. 사용자가 직접 로그인페이지를 요청한 경우
+			uri = prevPage;
+			log.info("prevPage : {}", prevPage);
+		}
+		log.info("요청 uri : {}", uri);
+		
+		/* 3-2. 회원 권한 및 요청 상황에 따른 기본 페이지 이동 */
 		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 		log.info("ROLE : {}", roles);
 		
 		if(roles.contains("ROLE_ADMIN")) { //관리자는 로그인 시 대시보드로 기본 이동
 			response.sendRedirect("/admin/dashboard");
+		} else if(roles.contains("ROLE_MEMBER") && uri != "") { //사이트 이용 중 요구된 로그인 인증을 마친 회원의 경우 이전 페이지로 이동
+			response.sendRedirect(uri);
 		} else {
 			response.sendRedirect("/");
 		}
