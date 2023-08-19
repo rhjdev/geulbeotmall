0b9f38d9219bad53b6551434b1db11cec351bb9c
rhjdev <rhjdev@gmail.com>
2023-08-19 17:53:15 +0900

    Refactor: 심두께 Enum 필드 추가, 상품상세페이지 스타일 반영

--- geulbeotmall/src/main/java/com/reminder/geulbeotmall/product/model/dto/PointSize.java
+++ geulbeotmall/src/main/java/com/reminder/geulbeotmall/product/model/dto/PointSize.java
@@ -2,24 +2,30 @@ package com.reminder.geulbeotmall.product.model.dto;
 
 public enum PointSize {
 	
-	FIRST(0.3),
-	SECOND(0.4),
-	THIRD(0.5),
-	FOURTH(0.7),
-	FIFTH(0.9),
-	SIXTH(1.0),
-	SEVENTH(1.2),
-	EIGHTH(1.5),
-	NINTH(1.7),
-	TENTH(2.0);
+	FIRST(0.3, "height: 0.5px;"),
+	SECOND(0.4, "height: 0.7px;"),
+	THIRD(0.5, "height: 1.0px;"),
+	FOURTH(0.7, "height: 1.5px;"),
+	FIFTH(0.9, "height: 2.0px;"),
+	SIXTH(1.0, "height: 2.2px;"),
+	SEVENTH(1.2, "height: 2.5px;"),
+	EIGHTH(1.5, "height: 2.7px;"),
+	NINTH(1.7, "height: 3.0px;"),
+	TENTH(2.0, "height: 3.2px;");
 	
 	private final double value;
+	private final String thickness;
 	
-	PointSize(double value) {
+	PointSize(double value, String thickness) {
 		this.value = value;
+		this.thickness = thickness;
 	}
 	
 	public double getValue() {
 		return value;
+	}
+	
+	public String getThickness() {
+		return thickness;
 	}
 }
